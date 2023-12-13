4384c4fb74acd18db774b89c1272a0580ab24c13
Katya Laso <kat_jenny@hotmail.com>
2023-12-13 11:28:49 -0600

    mochawesome reporter added

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,16 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+
+    reporter: "mochawesome",
+    reporterOptions: {
+      reportDir: "cypress/reports/mocha",
+      overwrite: true,
+      html: true,
+      json: true,
+      video: true
+    },
+  
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
